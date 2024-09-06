148db84f0f208fcc88b6b7b11ac81bf4fca13631
kevin-kimutai <kevinkimutai62@gmail.com>
2024-09-06 14:03:31 +0300

    routes

--- src/main/java/com/ctrla/runners/run/RunController.java
+++ src/main/java/com/ctrla/runners/run/RunController.java
@@ -1,13 +1,23 @@
 package com.ctrla.runners.run;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping("/api/v1/runs")
 public class RunController {
 
-    @GetMapping("/hello")
-    public String hello() {
-        return "Waatu?,mnajuaje niaje?";
+    @GetMapping()
+    public String getAllRuns() {
+        //Return All Runs
+        return "/run:Get all runs route";
+    }
+
+    @GetMapping("/{run_id}")
+    public String getRunById(@PathVariable String run_id) {
+        //Return All Runs
+        return "/run/{run_id}: Get run by id";
     }
 }
