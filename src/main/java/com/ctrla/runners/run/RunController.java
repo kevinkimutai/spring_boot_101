8a82523de367ccfda42d1bf3addf1efe5a5db81d
kevin-kimutai <kevinkimutai62@gmail.com>
2024-09-10 11:54:13 +0300

    spring data

--- src/main/java/com/ctrla/runners/run/RunController.java
+++ src/main/java/com/ctrla/runners/run/RunController.java
@@ -1,23 +1,33 @@
 package com.ctrla.runners.run;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/api/v1/runs")
 public class RunController {
 
-    @GetMapping()
-    public String getAllRuns() {
-        //Return All Runs
-        return "/run:Get all runs route";
-    }
-
-    @GetMapping("/{run_id}")
-    public String getRunById(@PathVariable String run_id) {
-        //Return All Runs
-        return "/run/{run_id}: Get run by id";
-    }
+//    private final RunRepository runRepository;
+//
+//    public RunController(RunRepository runRepository) {
+//        this.runRepository = runRepository;
+//    }
+//
+//    @GetMapping()
+//    public List<Run> getAllRuns() {
+//      return  runRepository.findAllRuns();
+//    }
+//
+//    @ResponseStatus(HttpStatus.OK)
+//    @GetMapping("/{run_id}")
+//    public String getRunById(@PathVariable String run_id) {
+//        //Return All Runs
+//        return "/run/{run_id}: Get run by id";
+//    }
+//
+//    @ResponseStatus(HttpStatus.CREATED)
+//    @PostMapping("")
+//    void createRun(@RequestBody Run run) {}
 }
---
+++ src/main/java/com/ctrla/runners/run/RunRepository.java
@@ -0,0 +1,14 @@
+package com.ctrla.runners.run;
+
+import jakarta.annotation.PostConstruct;
+import org.springframework.stereotype.Repository;
+
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
+
+@Repository
+public class RunRepository {
+
+
+}
